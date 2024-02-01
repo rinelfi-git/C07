eed113da8d3a88d14b852263f2697f8d1ed98886
Elie Fidele Rijaniaina <erijania@c1r5s4.42antananarivo.mg>
2024-02-01 18:27:48 +0300

    ex02

--- ex02/ft_ultimate_range.c
+++ ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 22:47:51 by erijania          #+#    #+#             */
-/*   Updated: 2024/01/30 22:51:06 by erijania         ###   ########.fr       */
+/*   Updated: 2024/02/01 18:22:41 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,27 @@
 
 int	ft_ultimate_range(int **range, int min, int max)
 {
+	int	*ret;
+	int	size;
+	int	i;
+
+	if (min >= max)
+	{
+		range = 0;
+		return (0);
+	}
+	size = max - min;
+	ret = malloc(sizeof(int) * size);
+	if (ret)
+	{
+		i = 0;
+		while (i < size)
+		{
+			ret[i] = i + min;
+			i++;
+		}
+		range[0] = ret;
+		return (size);
+	}
 	return (-1);
 }
